642b84e6664d79681ca44dba7ad8c137a1de5dab
Ahmad Farid Shoja <ahmadfaridshoja@gmial.com>
2024-10-01 17:39:50 -0400

    first commit

---
+++ WhileHW.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int num=0;
+        while (num<=10) {
+            System.out.println(num);
+            num++;
+        }
+
+    }
+}
